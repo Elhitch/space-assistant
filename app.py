b0374d21022f83dd8da2ab19ab0834cec663ce41
DNSLV-PMTKV <pometkov.d@protonmail.com>
2019-12-13 20:53:16 +0200

    Added nltk and find the verb in the sentence

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from commands import *
+import nltk
 
 
 def find_verb(command: str) -> list:
+    """Get the verb out of the sentence."""
     command = command.strip()
-    return command.split(' ')
+    tokenized = nltk.word_tokenize(command)
+    print(nltk.pos_tag(tokenized))
+    is_verb = lambda pos: pos[:2] == 'VB'
+    verbs = [word for (word, pos) in nltk.pos_tag(tokenized) if is_verb(pos)]
+    return verbs
 
 
 def main():
     while True:
         command = input('> ')
-        print((command))
+        print(find_verb(command))
 
 
 if __name__ == "__main__":
