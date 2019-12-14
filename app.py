0bf2cf519d095494e6ed5c5f5141c976d2c161c5
DNSLV-PMTKV <pometkov.d@protonmail.com>
2019-12-14 15:35:35 +0200

    Restructure commands folder so our script be modules

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-from commands import *
 import nltk
+import json
+import importlib
 
 
-def find_verb(command: str) -> list:
+def find_nouns(command: str) -> list:
     """Get the verb out of the sentence."""
     command = command.strip()
     tokenized = nltk.word_tokenize(command)
-    print(nltk.pos_tag(tokenized))
-    is_verb = lambda pos: pos[:2] == 'VB'
-    verbs = [word for (word, pos) in nltk.pos_tag(tokenized) if is_verb(pos)]
-    return verbs
+    is_noun = lambda pos: pos[:2] == 'NN'
+    nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+    return nouns
+
+
+def find_module(noun: str) -> str:
+    """Find the matching module based on noun"""
+    with open('config.json') as f:
+        data = json.load(f)
+        # print(data['commands'])
+        for key, values in data['commands'].items():
+            if noun[0] in values:
+                return key
 
 
 def main():
     while True:
         command = input('> ')
-        print(find_verb(command))
+
+        nouns = find_nouns(command)
+        module_name = find_module(nouns)
+        module = importlib.import_module(f'commands.{module_name}')
+
+        module.initialize()
 
 
 if __name__ == "__main__":
