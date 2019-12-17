c4baa06f11276de66cb4f2a966469eba277b6024
Dimitar Georgiev <dgeorgiev@bulgariait.net>
2019-12-18 00:46:21 +0200

    Adding basic audio. Use './app.py -a'

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+import sys
 import nltk
 import json
 import importlib
+import speech_recognition as sr
 
 def find_nouns(command: str) -> list:
     """Get the verb out of the sentence."""
@@ -26,8 +28,31 @@ def find_module(nouns: str) -> str:
         return None
 
 def main():
+    inputFromAudio = False
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i] == "-a":
+            inputFromAudio = True
+
     while True:
-        command = input('> ')
+        if inputFromAudio:
+            saAudioInput = sr.Recognizer()  
+            with sr.Microphone() as source:
+                print("Please wait. Calibrating microphone...")
+                # Profile ambient noise for better accuracy
+                saAudioInput.adjust_for_ambient_noise(source, duration=5)
+                print("Say something!")
+                audio = saAudioInput.listen(source)
+            
+            # (Try to) recognize speech using Sphinx engine
+            try:  
+                command = saAudioInput.recognize_sphinx(audio)
+                print(command)
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e))
+        else:
+            command = input('> ')
 
         nouns = find_nouns(command)
         """ 
