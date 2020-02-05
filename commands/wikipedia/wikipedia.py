0ff5e34c08d052b8a2aa7ba08810f81d8a5b957a
DNSLV-PMTKV <pometkov.d@protonmail.com>
2020-02-05 12:13:28 +0200

    Added validation check on wikipedia command

--- commands/wikipedia/wikipedia.py
+++ commands/wikipedia/wikipedia.py
@@ -4,5 +4,10 @@ import wikipedia
 def initialize(sentence):
     sentence = ' '.join(word[0] for word in sentence)
     sentence = sentence.replace('wikipedia', '')
-    results = wikipedia.summary(sentence, sentences=2)
+    if not sentence:
+        return 'You didn\'t mention what you\'d like on Wikipedia.'
+    try:
+        results = wikipedia.summary(sentence, sentences=2)
+    except:
+        return 'Could not find results.'
     return f'According to Wikipedia: {results}'
